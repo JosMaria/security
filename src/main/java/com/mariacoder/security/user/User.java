358f4f9ea8b2d4e06c98eaebff011c50f7f9c5cb
JoseMaria <josmariaguilar@gmail.com>
2023-05-03 09:45:08 -0400

    configure database and create entity user

---
+++ src/main/java/com/mariacoder/security/user/User.java
@@ -0,0 +1,27 @@
+package com.mariacoder.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firtname;
+    private String lastname;
+    private String email;
+    private String password;
+}
